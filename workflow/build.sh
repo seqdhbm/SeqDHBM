228a1fa76a3480756eabc1ab46eaab936a06f59b
Ak imhof <mauriciopl@gmail.com>
2018-11-08 16:23:16 +0100

    Scripts to compile/deploy the website

---
+++ workflow/build.sh
@@ -0,0 +1,15 @@
+# convert the notebooks into actual python modules
+jupyter nbconvert --to script workflow.ipynb
+jupyter nbconvert --to script experiments.ipynb
+jupyter nbconvert --to script fasta.ipynb
+jupyter nbconvert --to script pdbfiles.ipynb
+jupyter nbconvert --to script ../../SeqD-HBM/SeqDHBM.ipynb
+
+# create a package for deployment
+mkdir seqdhbm
+mv *.py seqdhbm
+cp ../../SeqD-HBM/*.py seqdhbm
+cp ../../SeqD-HBM/wget.exe seqdhbm
+echo "__all__ = ['pdbfiles', 'SeqDHBM', 'workflow', 'fasta']" >> seqdhbm/__init__.py
+
+echo "Package created in ./seqdhbm"
---
+++ workflow/deploy.sh
@@ -0,0 +1,1 @@
+cp -aR seqdhbm ~/Public/git/hemesite/
