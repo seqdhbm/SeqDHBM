4991988577f63c5ac2dd4b24a1d509c39db794d2
AK IMHOF <mauriciopl@gmail.com>
2019-03-13 14:34:01 +0100

    updating the example for loca_settings.py

--- hemesite/hemesite/specificsettings.py
+++ hemesite/hemesite/specificsettings.py
@@ -1,4 +1,12 @@
+# coding: utf-8
+
+"""Installation specific variables and configurations that shouldn't be
+on version control"""
+
 SECRET_KEY = ""
 EMAIL_HOST_PASSWORD = ""
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+SITE_DOMAIN = ""
